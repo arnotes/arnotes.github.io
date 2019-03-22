979151d74be4a432af4d1ce719c73188aabbe20c
arnold_naval <arnold.naval99@gmail.com>
2019-03-21 22:04:11 -0600

    v2 initial deploy fix env

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6ea270cc0fe53cbaf88faf4f6251ce2.js"
+  "/precache-manifest.61a1a06097a498c94adfe6f3297e7159.js"
 );
 
 workbox.clientsClaim();
